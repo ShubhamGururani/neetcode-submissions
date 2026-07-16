395541ca29decb88b6974a9ce568cc374b06ccfc
Shubham Gururani <32897406+ShubhamGururani@users.noreply.github.com>
2026-07-16 14:44:46 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq = {}
+        for i in s:
+            if not freq.get(i):
+                freq[i]=1
+                continue
+            freq[i]+=1
+        for i in t:
+            if not freq.get(i) or freq[i]<1:
+                return False
+            freq[i]-=1
+        for i in freq.keys():
+            if freq[i]!=0:
+                return False
+        return True
+        
